6aaf84de4a9ab4e8bba720b7a89dcc865f5db18c
linprqwq797 <317348898@qq.com>
2022-04-11 18:09:01 +0800

    zc

--- RetailersBackSystem-controller/src/main/java/com/guigu/controller/RegisterController.java
+++ RetailersBackSystem-controller/src/main/java/com/guigu/controller/RegisterController.java
@@ -1,10 +1,17 @@
 package com.guigu.controller;
 
+import com.guigu.pojo.Userinfo;
 import com.guigu.service.RegisterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.type.StandardMethodMetadata;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
 
 @RestController
 
@@ -23,5 +30,21 @@ public class RegisterController {
     @CrossOrigin
     public int checkloginname(String loginname){
         return registerService.CheckLoginName(loginname);
+    }
+
+    @RequestMapping("/registeruser.action")
+    @CrossOrigin
+    public int RegisterUser(Userinfo userinfo ,MultipartFile[] file, HttpServletRequest request) throws IOException {
+        for (int i = 0; i < file.length; i++) {
+            String path = request.getServletContext().getRealPath("/image"); //路径名
+            String name = file[i].getOriginalFilename();  //文件名
+            File savefile = new File(path,name);
+            file[i].transferTo(savefile);
+            //组装商品图片实体类对象
+           userinfo.setImgpath("image/"+name);
+            System.out.println(name+"*********");
+
+        }
+        return registerService.RegisterUser(userinfo);
     }
 }
--- RetailersBackSystem-mapper/src/main/java/com/guigu/mapper/RegisterMapper.java
+++ RetailersBackSystem-mapper/src/main/java/com/guigu/mapper/RegisterMapper.java
@@ -3,6 +3,8 @@ package com.guigu.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.guigu.pojo.Userinfo;
 
+import java.util.List;
+
 public interface RegisterMapper extends BaseMapper<Userinfo> {
 
     //检查昵称是否重复
@@ -10,4 +12,7 @@ public interface RegisterMapper extends BaseMapper<Userinfo> {
 
     //检查登录账号是否重复
     Integer CheckLoginName(String loginnameqwq);
+
+    //注册
+    Integer RegisterUser(Userinfo userinfo);
 }
