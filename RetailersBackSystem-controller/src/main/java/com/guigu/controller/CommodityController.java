fed02fe20b1382c3d49d702fcc5b4fababc050e3
ngh17 <2838961043@qq.com>
2022-04-14 17:14:02 +0800

    添加商品

--- RetailersBackSystem-controller/src/main/java/com/guigu/controller/CommodityController.java
+++ RetailersBackSystem-controller/src/main/java/com/guigu/controller/CommodityController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
